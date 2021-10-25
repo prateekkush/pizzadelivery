687ec518b636b584c8ef495febb14a23571598fc
prateek363 <92567239+prateek363@users.noreply.github.com>
2021-10-25 07:06:45 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,118 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
+const Emitter = require('events')
+
+
+// Database Connection
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+
+
+
+//Session store
+
+let mongoStore = new MongoDbStore({
+                mongooseConnection:connection,
+                collection: 'sessions'
+})
+
+
+//Event Emitter
+
+const eventEmitter = new Emitter()
+app.set('eventEmitter', eventEmitter)
+
+
+
+//Session Config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24}
+}))
+
+
+//Passport Config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.use(flash())
+
+// Assets
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+//Global Middleware
+
+app.use((req, res, next) =>{
+    res.locals.session = req.session
+    res.locals.user = req.user
+    next()
+})
+
+
+// Set Template Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs')
+
+
+
+
+
+require('./routes/web')(app)
+app.use((req,res) =>{
+    res.status(404).render('errors/404')
+})
+
+
+
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
+})
+
+
+// Socket
+
+const io = require('socket.io')(server)
+
+io.on('connection', (socket)=>{
+   //Join
+    socket.on('join',(orderId)=>{
+        socket.join(orderId)
+    })
+})
+
+eventEmitter.on('orderUpdated', (data)=>{
+    io.to(`order_${data.id}`).emit('orderUpdated', data)
+
+})
+
+eventEmitter.on('orderPlaced', (data)=>{
+    io.to('adminRoom').emit('orderPlaced', data)
+})
